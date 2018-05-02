c6dbfbdd2c0a91df62743bb15150eca4083a8ae1
Austin Zanoni <austinzanoni21@gmail.com>
2018-05-02 17:50:45 -0600

    added almost all funtionality except for attack. Need to make isScared and surrender have a default function.

--- Final/Advneture.pl
+++ Final/Advneture.pl
@@ -1,19 +1,24 @@
 /* Text based adventure game. For Artificial Intelligence class.
    Even though it is a texted based adventure game I will implement in AI type behavior for the opponents.
-   By: Austin Zaoni
+   By: Austin Zanoni
 
    To start game input the command start. (with the period)
 */
 
 /* Defining the structure of the different "variables" I will be using later on  */
-:- dynamic at/2, i_am_at/1, alive/1.
-:- retractall(at(_,_)), retractall(i_am_at(_)), retractall(alive(_)).
+:- dynamic at/2, i_am_at/1, king_health/1, health/1, prisoner_health/1, scared/1, surrendered/1, prisoned/1.
+:- retractall(at(_,_)), retractall(i_am_at(_)), retractall(king_health(_)), retractall(health(_)), retractall(prisoner_health(_)), retractall(scared(_)), retractall(surrendered(_)), retractall(prisoned(_)).
 
 
-/* starting location */
+/* starting location and starting dynamic variables */
 
 i_am_at(field).
-
+king_health(unhurt).
+health(unhurt).
+prisoner_health(unhurt).
+scared(no).
+surrendered(no).
+prisoned(no).
 
 /*  Describing the different connections between locations   */
 
@@ -40,6 +45,9 @@ path(castle, s, armory) :-
   fail.
 path(armory, n, castle).
 path(castle, w, throne_room).
+path(throne_room, e, castle) :- surrendered(yes).
+path(throne_room, e, castle) :-
+  write('The door has been locked behind you.'), nl, fail.
 path(castle, u, upstairs).
 path(castle, d, basement).
 
@@ -59,7 +67,7 @@ path(bedroom, s, upstairs).
 path(basement, u, castle).
 path(basement, s, cage) :- at(bronze_key, in_hand).
 path(basement, s, cage) :-
-  write('The cage is locked.'), nl,
+  write('The cage appears to be locked.'), nl,
   fail.
 path(cage, n, basement).
 path(basement, w, table).
@@ -71,11 +79,12 @@ instructions :-
   nl, write('Please enter command to decide what you want to do.'), nl,
   write('Available commands are(make sure to put in periods):'), nl,
   write('north. south. west. east. up. down.  -- to go in that direction'), nl,  /* done */
-  write('take(item).                          -- to take available item'), nl,  /* not done bronze key with cage not working properly */
+  write('take(item).                          -- to take available item'), nl,  /* done */
   write('drop(item).                          -- to drop available items'), nl,  /* done */
-  write('interact.                            -- to do something with object'), nl,
+  write('interact.                            -- to do something with object'), nl,  /* done */
   write('observe.                             -- look around at settings'), nl,   /* done */
-  write('talk.                                -- talk to people around you'), nl, /* done with functionality just need to add correct dialoge */
+  write('talk.                                -- talk to people around you'), nl, /* done */
+  write('intimidate.                          -- make your opponent surrender'), nl, /* done */
   write('attack.                              -- attack person of intrest'), nl,
   write('instructions.                        -- show this message again'), nl,  /* done */
   write('halt.                                -- to exit game'), nl, nl.   /* done */
@@ -83,6 +92,182 @@ instructions :-
 start :-
   instructions,
   look.
+
+/* intimidate will make the king surrender if he is scared */
+
+intimidate :-
+  i_am_at(throne_room),
+  surrendered(no),
+  scared(yes),
+  retract(surrendered(no)),
+  assert(surrendered(yes)),
+  write('You have intimidated the king. He has thrown down his sword and surrendered to you.'), nl, !.
+
+intimidate :-
+  i_am_at(throne_room),
+  surrendered(yes),
+  write('The king has already surrendered to you.'), nl, !.
+
+intimidate :-
+  i_am_at(throne_room),
+  scared(no),
+  write('The king merely laughs at your attempt to intimidate a king as mighty as him.'), nl, !.
+
+intimidate :-
+  write('There is nothing here for you to intimidate.'), nl.
+
+/* isScared function to check to see if the king is scared */
+
+isScared :-     /* you and prisoner have sword */
+  at(steel_sword, in_hand),
+  prisoned(no),
+  at(sword, prisoner),
+  scared(no),
+  retract(scared(no)),
+  assert(scared(yes)).
+
+isScared :-     /* you have 2 swords and freed prisoner */
+  at(steel_sword, in_hand),
+  at(sword, in_hand),
+  prisoned(no),
+  scared(no),
+  retract(scared(no)),
+  assert(scared(yes)).
+
+isScared :-     /* you have freed the prisoner and have 1 sword */
+  at(steel_sword, in_hand),
+  prisoned(no),
+  scared(no),
+  king_health(serious),
+  retract(scared(no)),
+  assert(scared(yes)).
+
+isScared :-     /* you have freed the prisoner and have 1 sword */
+  at(sword, in_hand),
+  prisoned(no),
+  scared(no),
+  king_health(serious),
+  retract(scared(no)),
+  assert(scared(yes)).
+
+isScared :-     /* you have freed the prisoner and he has a sword */
+  at(sword, prisoner),
+  prisoned(no),
+  scared(no),
+  king_health(serious),
+  retract(scared(no)),
+  assert(scared(yes)).
+
+isScared :-     /* you have 2 swords and have not free the prisoner */
+  at(sword, in_hand),
+  at(steel_sword, in_hand),
+  prisoned(yes),
+  scared(no),
+  king_health(serious),
+  retract(scared(no)),
+  assert(scared(yes)).
+
+isScared :-     /* you have sword and have not freed the prisoner */
+  at(sword, in_hand),
+  prisoned(yes),
+  scared(no),
+  king_health(serious),
+  retract(scared(no)),
+  assert(scared(yes)).
+
+isScared :-     /* you have steel_sword and have not freed the prisoner */
+  at(steel_sword, in_hand),
+  prisoned(yes),
+  scared(no),
+  king_health(serious),
+  retract(scared(no)),
+  assert(scared(yes)).
+
+/* surrender function is when the king will surrender to you on his own without intimidation needed */
+
+surrender :-     /* you and the prisoner have a sword */
+  at(steel_sword, in_hand),
+  prisoned(no),
+  at(sword, prisoner),
+  scared(yes),
+  surrendered(no),
+  king_health(mortally_hurt),
+  retract(surrendered(no)),
+  assert(surrendered(yes)),
+  write('The king throws down his sword and surrenders to you. He acknowledges you to have bested him.'), nl.
+
+surrender :-     /* you have 2 swords and have freed the prisoner */
+  at(steel_sword, in_hand),
+  at(sword, in_hand),
+  prisoned(no),
+  scared(yes),
+  surrendered(no),
+  king_health(mortally_hurt),
+  retract(surrendered(no)),
+  assert(surrendered(yes)),
+  write('The king throws down his sword and surrenders to you. He acknowledges you to have bested him.'), nl.
+
+surrender :-     /* you have a steel_swords and have freed the prisoner */
+  at(steel_sword, in_hand),
+  prisoned(no),
+  scared(yes),
+  surrendered(no),
+  king_health(mortally_hurt),
+  retract(surrendered(no)),
+  assert(surrendered(yes)),
+  write('The king throws down his sword and surrenders to you. He acknowledges you to have bested him.'), nl.
+
+surrender :-     /* you have a sword and have freed the prisoner */
+  at(sword, in_hand),
+  prisoned(no),
+  scared(yes),
+  surrendered(no),
+  king_health(mortally_hurt),
+  retract(surrendered(no)),
+  assert(surrendered(yes)),
+  write('The king throws down his sword and surrenders to you. He acknowledges you to have bested him.'), nl.
+
+surrender :-     /* you have freed the prisoner and he has a sword */
+  at(sword, prisoner),
+  prisoned(no),
+  scared(yes),
+  surrendered(no),
+  king_health(mortally_hurt),
+  retract(surrendered(no)),
+  assert(surrendered(yes)),
+  write('The king throws down his sword and surrenders to you. He acknowledges you to have bested him.'), nl.
+
+surrender :-     /* you have 2 swords and have not freed the prisoner */
+  at(sword, in_hand),
+  at(steel_sword, in_hand),
+  prisoned(yes),
+  scared(yes),
+  surrendered(no),
+  king_health(mortally_hurt),
+  retract(surrendered(no)),
+  assert(surrendered(yes)),
+  write('The king throws down his sword and surrenders to you. He acknowledges you to have bested him.'), nl.
+
+surrender :-     /* you have a sword and have not freed the prisoner */
+  at(sword, in_hand),
+  prisoned(yes),
+  scared(yes),
+  surrendered(no),
+  king_health(mortally_hurt),
+  retract(surrendered(no)),
+  assert(surrendered(yes)),
+  write('The king throws down his sword and surrenders to you. He acknowledges you to have bested him.'), nl.
+
+surrender :-     /* you have a steel_swords and have not freed the prisoner */
+  at(steel_sword, in_hand),
+  prisoned(yes),
+  scared(yes),
+  surrendered(no),
+  king_health(mortally_hurt),
+  retract(surrendered(no)),
+  assert(surrendered(yes)),
+  write('The king throws down his sword and surrenders to you. He acknowledges you to have bested him.'), nl.
+
 
 /* go function is called it will see if the call matches any valid directions to move and change your location if it can */
 
@@ -116,10 +301,10 @@ observe :-
 
 /* setting locations of items to interact with */
 
-at(steel_sword, armory).
+at(steel_sword, in_hand).
 at(key, dresser).
 at(bronze_key, table).
-at(sword, store).
+at(sword, in_hand) /*:- at(coins, store_owner)*/.
 at(coins, village_center).
 
 /* Picking up and dropping items */
@@ -131,12 +316,27 @@ take(X) :-
 take(X) :-
   i_am_at(Location),
   at(X, Location),
-  retract(at(X, Location)),
+  retractall(at(X, Location)),
   assert(at(X, in_hand)),
   write('Item picked up.'), nl, !.
 
+
 take(_) :-
   write('You do not see that item.'), nl.
+
+drop(sword) :-
+  at(sword, in_hand),
+  i_am_at(cage),
+  retract(at(sword, in_hand)),
+  assert(at(sword, prisoner)),
+  write('You gave the prisoner the sword.'), nl, !.
+
+drop(coins) :-
+  at(coins, in_hand),
+  i_am_at(store),
+  retract(at(coins, in_hand)),
+  assert(at(coins, store_owner)),
+  write('You gave the owner the coins.'), nl, !.
 
 drop(X) :-
   at(X, in_hand),
@@ -156,16 +356,49 @@ talk :-
 
 conversation(village_center) :-
   write('Please you seem like a strong adventurer could you find my son for me? I am'), nl,
-  write('willing to help you out with some money so you can go and defeat the king.'), nl, !.
+  write('willing to help you out with some coins so you can go and defeat the king.'), nl,
+  write('Please take these coins and then go to the store and drop of the coins so you'), nl,
+  write('so you can take a sword.'), nl, !.
 
 conversation(cage) :-
-  write('Thank you so much for saving me! Let us go together and kill the king!'), nl, !.
+  write('Thank you so much for saving me! Let us go together and kill the king! If you'), nl,
+  write('have an extra sword just drop it for me and we can fight him together!'), nl, !.
 
 conversation(throne_room) :-
-  write('I am king BAAAAAHHHHHHH'), nl, !.
+  write('You dare come into my throne room. BBWWAAHAHAHAHAHAHAHAHHAHAHAHAHAHAHHAHAHAHAHHA!'), nl,
+  write('You come here thinking you can defeat me. You are a small worthless human who will'), nl,
+  write('fall by my blade!'), nl, !.
 
 conversation(_) :-
   write('There is no one for you to talk to here.'), nl.
+
+/* interact with different objects depending on where you are */
+
+interact :-
+  i_am_at(library),
+  write('You pick up the one book that appears to be most recently used. You sit down in a'), nl,
+  write('dusty armchair and begin to read the book. As you continue to read the book you being'), nl,
+  write('to figure that the book is about human sacrafice and how it can be used to become more'), nl,
+  write('powerful and extend your life for many years.'), nl, !.
+
+interact :-
+  i_am_at(bed),
+  write('You climb onto the bed. You begin to feel your body being dragged farther down into the'), nl,
+  write('sheets. Your eyes become heavy, a yawn escapes you....'), nl, nl,
+  write('You awake not knowing how much time has passed. You feel refreshed and prepared to take on'), nl,
+  write('anything that comes at you.'), nl, !.
+
+interact :-
+  i_am_at(kitchen),
+  write('The smell of the pastries begin to over take your senses. You grasp the nearest one, what'), nl,
+  write('looks to be a type of multiple berry tart. At first bite you realize how long it had been'), nl,
+  write('since the last time you had eaten and how delicious this tastes. Before you know it the tart'), nl,
+  write('is completly consumed. Still, your body longs for more of the delicate sweets. One after'), nl,
+  write('another you begin to eat more and more of the pastries until finnaly your stomach has been'), nl,
+  write('satisfied and you are only left with the amazing feeling of a full stomach.'), nl, !.
+
+interact() :-
+  write('There is nothing to interact with here.'), nl.
 
 /* Descriptions of all locations */
 
@@ -218,7 +451,7 @@ describe(castle) :-
   write('of the field of wild flower to the north.'), nl.
 
 describe(armory) :-
-  write('You open the lock into the armory and see great long sword hanging on the'), nl,
+  write('You open the lock into the armory and see great long steel_sword hanging on the'), nl,
   write('wall. You also can return to the castle entrance to the north.'), nl.
 
 describe(kitchen) :-
@@ -229,7 +462,8 @@ describe(kitchen) :-
 describe(throne_room) :-
   write('As you push open and walk through the huge doors you see a man all in gold'), nl,
   write('cloth lounging on the throne. You continue to walk through the doors and as you'), nl,
-  write('do you hear the great doors slam shut behind you.'), nl.
+  write('do you hear the great doors slam shut behind you.'), nl,
+  isScared.
 
 describe(upstairs) :-
   write('At the top of the stiars you see to the north what appears to be an entrance to'), nl,
@@ -273,10 +507,178 @@ describe(basement) :-
 
 describe(table) :-
   write('On the table you see different crude instruments covered in slightly dried blood.'), nl,
-  write('You also see a bronze key lying on the table. You can also return back to the center'), nl,
+  write('You also see a bronze_key lying on the table. You can also return back to the center'), nl,
   write('of the basement.'), nl.
 
-descibe(cage) :-
+describe(cage) :-
   write('As you approach the cage with the brozne key the man inside begins to cry knowing that'), nl,
   write('he has been saved. You unlock the cage for the man. He stares at you expectantly. You'), nl,
-  write('can also return to the center of the basement.'), nl.
+  write('can also return to the center of the basement.'), nl,
+  retract(prisoned(yes)),
+  assert(prisoned(no)).
+
+/* when you attack what will happen and what actions will the king take some form of ai here */
+
+/* Attack sequence of you have a sword and freed the prisoner and he has a sword */
+attack :-
+  i_am_at(throne_room),
+  at(steel_sword, in_hand),
+  prisoned(no),
+  at(sword, prisoner),
+  king_health(unhurt),
+  retract(king_health(unhurt)),
+  assert(king_health(mortally_hurt)),
+  isScared,
+  surrender, !.
+attack :-
+  i_am_at(throne_room),
+  at(steel_sword, in_hand),
+  prisoned(no),
+  at(sword, prisoner),
+  king_health(mortally_hurt),
+  retract(king_health(mortally_hurt)),
+  assert(king_health(dead)),
+  isScared,
+  surrender, !.
+/* Attack sequence of having 2 swords and freed prisoner */
+attack :-
+  write('heello'), 
+  i_am_at(throne_room),
+  at(steel_sword, in_hand),
+  at(sword, in_hand),
+  prisoned(no),
+  king_health(unhurt),
+  retract(king_health(unhurt)),
+  assert(king_health(mortally_hurt)),
+  isScared,
+  surrender, !.
+attack :-
+  i_am_at(throne_room),
+  at(steel_sword, in_hand),
+  at(sword, in_hand),
+  prisoned(no),
+  king_health(mortally_hurt),
+  retract(king_health(mortally_hurt)),
+  assert(king_health(dead)),
+  isScared,
+  surrender, !.
+/* Attack sequence of having a steel_sword and freed the prisoner */
+attack :-
+  i_am_at(throne_room),
+  at(steel_sword, in_hand),
+  prisoned(no),
+  king_health(unhurt),
+  retract(king_health(unhurt)),
+  assert(king_health(injured)),
+  isScared,
+  surrender, !.
+attack :-
+  i_am_at(throne_room),
+  at(steel_sword, in_hand),
+  prisoned(no),
+  king_health(injured),
+  retract(king_health(injured)),
+  assert(king_health(serious)),
+  isScared,
+  surrender, !.
+attack :-
+  i_am_at(throne_room),
+  at(steel_sword, in_hand),
+  prisoned(no),
+  king_health(serious),
+  retract(king_health(serious)),
+  assert(king_health(mortally_hurt)),
+  isScared,
+  surrender, !.
+attack :-
+  i_am_at(throne_room),
+  at(steel_sword, in_hand),
+  prisoned(no),
+  king_health(mortally_hurt),
+  retract(king_health(mortally_hurt)),
+  assert(king_health(dead)),
+  retract(prisoner_health(unhurt)),
+  assert(prisoner_health(dead)),
+  isScared,
+  surrender, !.
+/* Attack sequence of having a sword and freed the prisoner */
+attack :-
+  i_am_at(throne_room),
+  at(sword, in_hand),
+  prisoned(no),
+  king_health(unhurt),
+  retract(king_health(unhurt)),
+  assert(king_health(injured)),
+  isScared,
+  surrender, !.
+attack :-
+  i_am_at(throne_room),
+  at(sword, in_hand),
+  prisoned(no),
+  king_health(injured),
+  retract(king_health(injured)),
+  assert(king_health(serious)),
+  isScared,
+  surrender, !.
+attack :-
+  i_am_at(throne_room),
+  at(sword, in_hand),
+  prisoned(no),
+  king_health(serious),
+  retract(king_health(serious)),
+  assert(king_health(mortally_hurt)),
+  isScared,
+  surrender, !.
+attack :-
+  i_am_at(throne_room),
+  at(sword, in_hand),
+  prisoned(no),
+  king_health(mortally_hurt),
+  retract(king_health(mortally_hurt)),
+  assert(king_health(dead)),
+  retract(prisoner_health(unhurt)),
+  assert(prisoner_health(dead)),
+  isScared,
+  surrender, !.
+/* Attack sequence of having freed the prisoner and he has a sword */
+attack :-
+  i_am_at(throne_room),
+  at(sword, prisoner),
+  prisoned(no),
+  king_health(unhurt),
+  retract(king_health(unhurt)),
+  assert(king_health(injured)),
+  isScared,
+  surrender, !.
+attack :-
+  i_am_at(throne_room),
+  at(sword, prisoner),
+  prisoned(no),
+  king_health(injured),
+  retract(king_health(injured)),
+  assert(king_health(serious)),
+  isScared,
+  surrender, !.
+attack :-
+  i_am_at(throne_room),
+  at(sword, prisoner),
+  prisoned(no),
+  king_health(serious),
+  retract(king_health(serious)),
+  assert(king_health(mortally_hurt)),
+  isScared,
+  surrender, !.
+attack :-
+  i_am_at(throne_room),
+  at(sword, prisoner),
+  prisoned(no),
+  king_health(mortally_hurt),
+  retract(king_health(mortally_hurt)),
+  assert(king_health(dead)),
+  retract(prisoner_health(unhurt)),
+  assert(prisoner_health(dead)),
+  isScared,
+  surrender, !.
+
+attack :-
+  write('There is nothing to attack'), nl.
